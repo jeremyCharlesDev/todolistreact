aaf6c344481cc8375b3d8fdaeba18afd71850ec1
jeremyCharlesDev <jrmprod@gmail.com>
2020-10-13 15:53:23 +0200

    Updates

---
+++ precache-manifest.a806467a7e2a4f546266fcd8f6db3b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9ee7a3e601ad2c6757527244c8fa8f",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolistreact/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolistreact/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9e5c253ac3102be0d3",
+    "url": "/todolistreact/static/js/main.8ac12710.chunk.js"
+  },
+  {
+    "revision": "d8463f5520392ee25265",
+    "url": "/todolistreact/static/js/runtime-main.61eea731.js"
+  }
+]);
